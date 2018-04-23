1e17c2d8b8be66ed2f7c8997ae7d54a4ced7c105
Raymart De Guzman <raymart54@gmail.com>
2018-04-23 01:06:50 -0400

    CustomerHandler: test get single row

--- tests/CustomerHandlerTest.php
+++ tests/CustomerHandlerTest.php
@@ -10,4 +10,11 @@ class CustomerHandlerTest extends PHPUnit_Framework_TestCase
         $this->assertNotEmpty($ch->getAllCustomer());
         $this->assertNotNull($ch->getAllCustomer());
     }
+
+    public function testGetSingleRow()
+    {
+        $ch = new CustomerHandler();
+        $this->assertEquals(1, count($ch->getSingleRow("admin@gmail.com")));
+        $this->assertNotEquals(2, count($ch->getSingleRow("admin@gmail.com")));
+    }
 }
