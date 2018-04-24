4466e38cd986fa6e72410927d56c900b55a1a62e
Raymart De Guzman <raymart54@gmail.com>
2018-04-23 22:29:03 -0400

    unit test continue

--- tests/CustomerHandlerTest.php
+++ tests/CustomerHandlerTest.php
@@ -17,4 +17,29 @@ class CustomerHandlerTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(1, count($ch->getSingleRow("admin@gmail.com")));
         $this->assertNotEquals(2, count($ch->getSingleRow("admin@gmail.com")));
     }
+
+    public function testGetCustomerObj()
+    {
+        $ch = new CustomerHandler();
+        $email = "admin@gmail.com";
+        $this->assertNotEmpty($ch->getCustomerObj($email));
+        $customerEmail = $ch->getCustomerObj($email)->getEmail();
+        $this->assertEquals("admin@gmail.com", $customerEmail);
+    }
+
+    public function testGetCustomerObjByCid()
+    {
+        $nonExistentId = 9999;
+        $ch = new CustomerHandler();
+        $this->assertNull($ch->getCustomerObjByCid($nonExistentId)->getEmail());
+        $this->assertEmpty($ch->getCustomerObjByCid($nonExistentId)->getEmail());
+    }
+
+    public function testGetUsername()
+    {
+        $ch = new CustomerHandler();
+        $email = "test@gmail.com";
+        $username = $ch->getUsername($email);
+        $this->assertEquals("test", $username);
+    }
 }
