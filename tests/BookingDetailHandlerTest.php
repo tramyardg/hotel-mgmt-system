bb714011d1ebb056374039624c4743c83a1d99d0
Raymart De Guzman <raymart54@gmail.com>
2018-04-27 23:02:12 -0400

    BookingDetailHandler testGetAllBookings unit test

--- tests/BookingDetailHandlerTest.php
+++ tests/BookingDetailHandlerTest.php
@@ -1,11 +1,26 @@
 <?php
 require './vendor/autoload.php';
+require './app/models/RequirementEnum.php';
+require './app/models/StatusEnum.php';
 
 class BookingDetailHandlerTest extends PHPUnit_Framework_TestCase
 {
     public function testGetAllBookings()
     {
+        $faker = Faker\Factory::create();
+        $r = new Reservation();
+        $r->setChildren(1);
+        $r->setAdults(1);
+        $r->setRequests($faker->text(20));
+        $r->setTimestamp($faker->unixTime);
+        $r->setRequirement($r->requirement()[\models\RequirementEnum::NON_SMOKING]);
+        $r->setStatus($r->getStatus()[\models\StatusEnum::PENDING]);
+        $brh = new BookingReservationHandler($r);
+        $brh->create();
 
+        $bdh = new BookingDetailHandler();
+        $this->assertNotNull($bdh->getAllBookings());
+        $this->assertNotEmpty($bdh->getAllBookings());
     }
 
     public function testGetCustomerBookings()
