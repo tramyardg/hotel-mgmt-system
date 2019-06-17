92eb7a807ac1d6944d19ddabed292dfe4fc9d176
Raymart De Guzman <tramyardg@gmail.com>
2019-06-16 22:54:18 -0400

    admin dao unit test created

---
+++ tests/AdminDAOTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use dao\AdminDAO;
+
+class AdminDAOTest extends PHPUnit_Framework_TestCase
+{
+    public function testFetchByEmail()
+    {
+
+    }
+
+    public function testFetchByAdminId()
+    {
+
+    }
+
+    public function testFetchAll()
+    {
+
+    }
+}
