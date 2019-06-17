7ba25376e63609ebc6324248b43a8de9f67ac3e3
Raymart De Guzman <tramyardg@gmail.com>
2019-06-16 21:49:02 -0400

    created Admin class

---
+++ app/models/Admin.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace models;
+
+
+class Admin
+{
+    private $adminId;
+    private $fullname;
+    private $password;
+    private $email;
+    private $phone;
+
+    public function getAdminId()
+    {
+        return $this->adminId;
+    }
+
+    public function setAdminId($adminId)
+    {
+        $this->adminId = $adminId;
+    }
+
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    public function setFullname($fullname)
+    {
+        $this->fullname = $fullname;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+}
