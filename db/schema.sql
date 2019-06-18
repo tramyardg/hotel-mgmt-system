2f6c0c7489787a64d36f64dad56c07e5abdcd2bc
Raymart De Guzman <tramyardg@gmail.com>
2019-06-17 21:18:12 -0400

    minor change in schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE `booking`
 (
     `id`     int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `cid`    int(11)             NOT NULL,
-    `status` varchar(100) DEFAULT 'pending',
+    `status` varchar(100) DEFAULT 'pending', ## use enum here?
     `notes`  varchar(500) DEFAULT NULL
 );
 
@@ -12,16 +12,16 @@ CREATE TABLE `customer`
     `fullname` varchar(100)        NOT NULL,
     `email`    varchar(50)         NOT NULL,
     `password` varchar(150)        NOT NULL,
-    `phone`    varchar(25)         NOT NULL
+    `phone`    varchar(25)         DEFAULT NULL
 );
 
 CREATE TABLE administrator
 (
     adminId  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    fullname VARCHAR(100)    NOT NULL,
+    fullname VARCHAR(100)    DEFAULT NULL,
     password VARCHAR(100)    NOT NULL,
     email    VARCHAR(30)     NOT NULL,
-    phone    VARCHAR(25)     NOT NULL
+    phone    VARCHAR(25)     DEFAULT NULL
 );
 
 CREATE TABLE `reservation`
@@ -30,7 +30,7 @@ CREATE TABLE `reservation`
     `start`       varchar(30)         NOT NULL,
     `end`         varchar(30)         NOT NULL,
     `type`        varchar(100)        NOT NULL,
-    `requirement` varchar(100)                 DEFAULT 'no preference',
+    `requirement` varchar(100)                 DEFAULT 'no preference', ## use enum here?
     `adults`      int(2)              NOT NULL,
     `children`    int(2)                       DEFAULT '0',
     `requests`    varchar(500)                 DEFAULT NULL,
