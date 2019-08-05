42e2f99352e2ba0ce4215ab89e0fad43f5fdd582
Raymart De Guzman <tramyardg@gmail.com>
2019-08-04 21:31:31 -0400

    requirement enum: added room type

--- app/models/RequirementEnum.php
+++ app/models/RequirementEnum.php
@@ -5,7 +5,15 @@ namespace models;
 
 abstract class RequirementEnum
 {
-    const NO_PREFERENCE = 0;
-    const NON_SMOKING   = 1;
-    const SMOKING       = 2;
+    // const NO_PREFERENCE = 0;
+    // const NON_SMOKING   = 1;
+    // const SMOKING       = 2;
+
+    const NO_PREFERENCE = "NO PREFERENCE";
+    const NON_SMOKING   = "NON SMOKING";
+    const SMOKING       = "SMOKING";
+
+    const SINGLE = "Single";
+    const DOUBLE = "Double";
+    const DELUXE = "Deluxe";
 }
