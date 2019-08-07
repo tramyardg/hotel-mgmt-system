0c758cdecff8131920a7ba7de430a266ce03f248
Raymart De Guzman <tramyardg@gmail.com>
2019-08-06 21:14:29 -0400

    updated schema with using enum data type, tested in phpmyadmin

--- db/schema.sql
+++ db/schema.sql
@@ -2,17 +2,8 @@ CREATE TABLE `booking`
 (
     `id`     int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `cid`    int(11)             NOT NULL,
-    `status` varchar(100) DEFAULT 'pending', ## use enum here?
-    `notes`  varchar(500) DEFAULT NULL
-);
-
-# propose schema for booking
-CREATE TABLE `booking1`
-(
-    `id`     int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `cid`    int(11)             NOT NULL,
-    `status` ENUM('PENDING', 'CONFIRMED', 'CANCELLED') DEFAULT 'PENDING',
-    `notes`  varchar(500) DEFAULT NULL
+    `status` ENUM ('PENDING', 'CONFIRMED', 'CANCELLED') DEFAULT 'PENDING',
+    `notes`  varchar(500)                               DEFAULT NULL
 );
 
 CREATE TABLE `customer`
@@ -21,16 +12,16 @@ CREATE TABLE `customer`
     `fullname` varchar(100)        NOT NULL,
     `email`    varchar(50)         NOT NULL,
     `password` varchar(150)        NOT NULL,
-    `phone`    varchar(25)         DEFAULT NULL
+    `phone`    varchar(25) DEFAULT NULL
 );
 
 CREATE TABLE administrator
 (
-    adminId  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    fullname VARCHAR(100)    DEFAULT NULL,
-    password VARCHAR(100)    NOT NULL,
-    email    VARCHAR(30)     NOT NULL UNIQUE,
-    phone    VARCHAR(25)     DEFAULT NULL
+    `adminId`  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `fullname` VARCHAR(100) DEFAULT NULL,
+    `password` VARCHAR(100)    NOT NULL,
+    `email`    VARCHAR(30)     NOT NULL UNIQUE,
+    `phone`    VARCHAR(25)  DEFAULT NULL
 );
 
 CREATE TABLE `reservation`
@@ -38,11 +29,22 @@ CREATE TABLE `reservation`
     `id`          int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `start`       varchar(30)         NOT NULL,
     `end`         varchar(30)         NOT NULL,
-    `type`        varchar(100)        NOT NULL,
-    `requirement` varchar(100)                 DEFAULT 'no preference', ## use enum here?
+    `type`        ENUM ('Single', 'Double', 'Deluxe')              DEFAULT 'Single',
+    `requirement` ENUM ('No Preference', 'Non Smoking', 'Smoking') DEFAULT 'No Preference',
     `adults`      int(2)              NOT NULL,
-    `children`    int(2)                       DEFAULT '0',
-    `requests`    varchar(500)                 DEFAULT NULL,
-    `timestamp`   timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `hash`        varchar(100)                 DEFAULT NULL
+    `children`    int(2)                                           DEFAULT '0',
+    `requests`    varchar(500)                                     DEFAULT NULL,
+    `timestamp`   timestamp           NOT NULL                     DEFAULT CURRENT_TIMESTAMP,
+    `hash`        varchar(100)                                     DEFAULT NULL
 );
+
+# Constraints
+--
+ALTER TABLE `booking`
+    ADD CONSTRAINT `booking_customer__fk` FOREIGN KEY (`cid`) REFERENCES `customer` (`cid`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+    ADD CONSTRAINT `reservation_booking__fk` FOREIGN KEY (`id`) REFERENCES `booking` (`id`) ON DELETE CASCADE;
