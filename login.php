2c9e41edb915f7c2dbfd4d83f5f405d26fe5d1dc
Raymart De Guzman <tramyardg@gmail.com>
2019-08-12 22:01:24 -0400

    facebook login api php version

--- login.php
+++ login.php
@@ -73,6 +73,7 @@
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function (response) {
+      console.log('response', response);
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
           'Thanks for logging in, ' + response.name + '!';
@@ -89,6 +90,7 @@
     FB.logout(function (response) {
       console.log('response', response);
       console.log('successfully logout');
+      location.reload();
     });
   }
 
