6b9e8bc589addcaaebea69b8de9a16a2a273684c
Raymart De Guzman <tramyardg@gmail.com>
2019-09-17 21:27:09 -0400

    moved multi step rsvn js to separate file

--- index.php
+++ index.php
@@ -572,45 +572,7 @@ session_start();
           modal.find('.modal-body select#roomType').val(roomType);
         });
     });
-    // rsvn multi steps
-    let currentTab = 0;
-    showTab(currentTab);
-
-    function showTab(n) {
-      let x = document.getElementsByClassName("rsvnTab");
-      x[n].style.display = "block";
-
-      if (n === 0) {
-        document.getElementById("rsvnPrevBtn").style.display = "none";
-      } else {
-        document.getElementById("rsvnPrevBtn").style.display = "inline";
-      }
-      if (n === (x.length - 1)) {
-        document.getElementById("rsvnNextBtn").innerHTML = "Submit";
-      } else {
-        document.getElementById("rsvnNextBtn").innerHTML = "Next";
-      }
-      fixStepIndicator(n);
-    }
-
-    function fixStepIndicator(n) {
-      let i, x = document.getElementsByClassName("step");
-      for (i = 0; i < x.length; i++) {
-        x[i].className = x[i].className.replace(" active", "");
-      }
-      x[n].className += " active";
-    }
-
-    function rsvnNextPrev(n) {
-      let x = document.getElementsByClassName("rsvnTab");
-      // if (n === 1) return false;
-      // Hide the current tab:
-      x[currentTab].style.display = "none";
-      currentTab = currentTab + n;
-
-      showTab(currentTab);
-
-    }
 </script>
+<script src="js/multiStepsRsvn.js"></script>
 </body>
 </html>
---
+++ js/multiStepsRsvn.js
@@ -0,0 +1,38 @@
+// rsvn multi steps
+let currentTab = 0;
+showTab(currentTab);
+
+function showTab (n) {
+  let x = document.getElementsByClassName('rsvnTab');
+  x[n].style.display = 'block';
+
+  if (n === 0) {
+    document.getElementById('rsvnPrevBtn').style.display = 'none';
+  } else {
+    document.getElementById('rsvnPrevBtn').style.display = 'inline';
+  }
+  if (n === (x.length - 1)) {
+    document.getElementById('rsvnNextBtn').innerHTML = 'Submit';
+  } else {
+    document.getElementById('rsvnNextBtn').innerHTML = 'Next';
+  }
+  fixStepIndicator(n);
+}
+
+function fixStepIndicator (n) {
+  let i, x = document.getElementsByClassName('step');
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(' active', '');
+  }
+  x[n].className += ' active';
+}
+
+function rsvnNextPrev (n) {
+  let x = document.getElementsByClassName('rsvnTab');
+  // if (n === 1) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = 'none';
+  currentTab = currentTab + n;
+
+  showTab(currentTab);
+}
