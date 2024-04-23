8ad233d970985a1b600b55ef5bcc4545696e6656
Raymart DG <tramyardg@gmail.com>
2024-04-23 10:14:54 -0400

    update sql dump

--- hotel.sql
+++ hotel.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 24, 2018 at 08:49 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Host: localhost:8889
+-- Generation Time: Apr 23, 2024 at 10:14 AM
+-- Server version: 5.7.34
+-- PHP Version: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,18 +30,24 @@ SET time_zone = "+00:00";
 CREATE TABLE `booking` (
   `id` int(11) UNSIGNED NOT NULL,
   `cid` int(11) UNSIGNED NOT NULL,
-  `status` varchar(100) DEFAULT 'pending',
-  `notes` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `status` varchar(100) COLLATE utf8mb4_unicode_520_ci DEFAULT 'pending',
+  `notes` varchar(500) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 --
 -- Dumping data for table `booking`
 --
 
 INSERT INTO `booking` (`id`, `cid`, `status`, `notes`) VALUES
-  (12, 10, 'confirmed', NULL),
-  (13, 10, 'confirmed', NULL),
-  (14, 10, 'confirmed', NULL);
+(12, 10, 'CONFIRMED', NULL),
+(13, 10, 'confirmed', NULL),
+(14, 10, 'CANCELLED', NULL),
+(15, 13, 'CANCELLED', NULL),
+(16, 13, 'CONFIRMED', NULL),
+(17, 13, 'CANCELLED', NULL),
+(18, 13, 'PENDING', NULL),
+(19, 13, 'PENDING', NULL),
+(20, 13, 'PENDING', NULL);
 
 -- --------------------------------------------------------
 
@@ -52,21 +57,23 @@ INSERT INTO `booking` (`id`, `cid`, `status`, `notes`) VALUES
 
 CREATE TABLE `customer` (
   `cid` int(11) UNSIGNED NOT NULL,
-  `fullname` varchar(100) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(150) NOT NULL,
-  `phone` varchar(25) NOT NULL,
+  `fullname` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `password` varchar(150) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `phone` varchar(25) COLLATE utf8mb4_unicode_520_ci NOT NULL,
   `isadmin` int(1) UNSIGNED NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`cid`, `fullname`, `email`, `password`, `phone`,`isadmin`) VALUES
-  (10, 'Martha Smith', 'martha@hotmail.com', '$2y$10$L8elMrSO59YGZdGjnQxURuWK7FZUJpL8QgZPT7pKIwCu42PvU8Mm2', '5149991111',0),
-  (11, 'admin@gmail.com', 'admin@gmail.com', '$2y$10$nfud5jYwEnMmqv8YgUF3p.wh3EVGAONlRUUiu2TqFiNW.GsU6QKGm', '',1),
-  (12, 'admin@admin.com', 'admin@admin.com', '$2y$10$4FJtbVGCIpFnNxcDvSSXUueMESuDDoZvtygT/O4J9UHB1vfdO3Vza', '',1);
+INSERT INTO `customer` (`cid`, `fullname`, `email`, `password`, `phone`, `isadmin`) VALUES
+(10, 'Martha Smith', 'martha@hotmail.com', '$2y$10$L8elMrSO59YGZdGjnQxURuWK7FZUJpL8QgZPT7pKIwCu42PvU8Mm2', '5149991111', 0),
+(11, 'admin@gmail.com', 'admin@gmail.com', '$2y$10$nfud5jYwEnMmqv8YgUF3p.wh3EVGAONlRUUiu2TqFiNW.GsU6QKGm', '', 1),
+(12, 'admin@admin.com', 'admin@admin.com', '$2y$10$4FJtbVGCIpFnNxcDvSSXUueMESuDDoZvtygT/O4J9UHB1vfdO3Vza', '', 1),
+(13, 'leo raymart ssdllllllc', 'leo@gmail.com', '$2y$10$OCKnJrI.F.AkEX1kASFr8Os8CnlnT0OYIH4ypwR2x4FSnSsO8ZfdG', 'hello there how are you d', 0),
+(14, 'test', 'test@gmail.com', '$2y$10$NvBB7Yiejyp1ZoMn3KpQ6.E35OWVLspbvpUjfe/h.51gbajAJzub2', '', 0);
 
 -- --------------------------------------------------------
 
@@ -76,25 +83,31 @@ INSERT INTO `customer` (`cid`, `fullname`, `email`, `password`, `phone`,`isadmin
 
 CREATE TABLE `reservation` (
   `id` int(11) UNSIGNED NOT NULL,
-  `start` varchar(30) NOT NULL,
-  `end` varchar(30) NOT NULL,
-  `type` varchar(100) NOT NULL,
-  `requirement` varchar(100) DEFAULT 'no preference',
+  `start` varchar(30) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `end` varchar(30) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `type` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `requirement` varchar(100) COLLATE utf8mb4_unicode_520_ci DEFAULT 'no preference',
   `adults` int(2) NOT NULL,
   `children` int(2) DEFAULT '0',
-  `requests` varchar(500) DEFAULT NULL,
+  `requests` varchar(500) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `hash` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `hash` varchar(100) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 --
 -- Dumping data for table `reservation`
 --
 
 INSERT INTO `reservation` (`id`, `start`, `end`, `type`, `requirement`, `adults`, `children`, `requests`, `timestamp`, `hash`) VALUES
-  (12, '2018-05-09', '2018-05-11', 'double', 'non smoking', 2, 0, '', '2018-04-19 22:04:42', '5ad9127abbdf6'),
-  (13, '2018-04-24', '2018-04-25', 'deluxe', 'no preference', 1, 0, '', '2018-04-23 15:45:33', '5addff9dafa97'),
-  (14, '2018-04-27', '2018-04-30', 'deluxe', 'no preference', 1, 0, '', '2018-04-24 05:27:13', '5adec03166177');
+(12, '2018-05-09', '2018-05-11', 'double', 'non smoking', 2, 0, '', '2018-04-19 22:04:42', '5ad9127abbdf6'),
+(13, '2018-04-24', '2018-04-25', 'deluxe', 'no preference', 1, 0, '', '2018-04-23 15:45:33', '5addff9dafa97'),
+(14, '2018-04-27', '2018-04-30', 'deluxe', 'no preference', 1, 0, '', '2018-04-24 05:27:13', '5adec03166177'),
+(15, '2023-03-01', '2023-03-11', 'Single', 'non smoking', 3, 0, 'asd', '2023-02-26 20:42:39', '63fbc43fe4661'),
+(16, '2023-03-02', '2023-03-10', 'Double', 'no preference', 2, 0, 'alert(&quot;hello&quot;);', '2023-02-26 20:48:43', '63fbc5abf0486'),
+(17, '2023-02-27', '2023-03-11', 'Double', 'no preference', 2, 0, '', '2023-02-26 22:11:01', '63fbd8f561240'),
+(18, '2023-02-27', '2023-03-05', 'Single', 'no preference', 1, 0, '', '2023-02-26 22:29:00', '63fbdd2c9e8dc'),
+(19, '2023-02-26', '2023-03-11', 'Single', 'no preference', 1, 0, '', '2023-02-27 01:18:37', '63fc04ed64776'),
+(20, '2023-02-26', '2023-03-11', 'Double', 'no preference', 1, 0, '', '2023-02-27 04:34:17', '63fc32c9c651c');
 
 --
 -- Indexes for dumped tables
@@ -129,13 +142,13 @@ ALTER TABLE `reservation`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `cid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `cid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
